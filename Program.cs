4fcd24d143125bcf22a3aa0f3321db13c5c81834
Daniel Girón <danielchabaco@gmail.com>
2023-08-11 11:44:27 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_1_ejercicio_1
+{
+    internal class Program
+    {
+        static void faren()
+        {
+            Double cel, far, kel;
+            //Aparecer en pantalla
+            Console.WriteLine("Escriba los grados celsius ");
+            //Se leen los grados celsius y se convierten a double
+            cel = Convert.ToDouble(Console.ReadLine());
+            //Se obtienen los grados Fahrenheit
+            far = cel * 9.0 / 5.0 + 32;
+            //Se muestran los grados Fahrenheit
+            Console.WriteLine("{0} grados Celsius son {1} grados Fahrenheit", cel, far);
+            {
+                static void Celsi()
+            }
+              
+              
+            
+        }
+    }
+}
